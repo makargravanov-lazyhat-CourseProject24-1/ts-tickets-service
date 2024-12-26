5ce7eacf586b687c6ba35cff7dd381f078c52069
lazyhat <sevapetrov2016p@gmail.com>
2024-12-26 17:15:23 +0300

    deploy

--- src/main/kotlin/ru/jetlabs/ts/ticketsservice/TicketsServiceApplication.kt
+++ src/main/kotlin/ru/jetlabs/ts/ticketsservice/TicketsServiceApplication.kt
@@ -1,11 +1,14 @@
 package ru.jetlabs.ts.ticketsservice
 
+import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
+@ImportAutoConfiguration(ExposedAutoConfiguration::class)
 class TicketsServiceApplication
 
 fun main(args: Array<String>) {
